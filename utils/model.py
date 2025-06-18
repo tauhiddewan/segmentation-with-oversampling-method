d80eda784d7a02f66b43c84544ff0966f80b74f6
tauhiddewan <dewan.tauhid.rahman@gmail.com>
2025-06-17 20:38:27 -0400

    added polypvt model

--- utils/model.py
+++ utils/model.py
@@ -1,6 +1,10 @@
+import sys 
 import torch
+import torch.nn as nn
 from dotenv import dotenv_values
 from segformer_pytorch import Segformer
+from functools import partial
+from .polyp_pvt import  PyramidVisionTransformerImpr, PolypPVT
 
 # N                     : heads of each stage, heads
 # E                     : feedforward expansion factor of each stage
@@ -78,9 +82,73 @@ unet_config = {
     'force_reload': True
 }
 
+
+
+polyppvt_b0_config = {
+    "patch_size" : 4, 
+    "embed_dims": [32, 64, 160, 256], 
+    "num_heads" : [1, 2, 5, 8], 
+    "mlp_ratios" : [8, 8, 4, 4],
+    "depths" : [2, 2, 2, 2], 
+    "sr_ratios" : [8, 4, 2, 1],
+    "num_classes":1
+}
+
+    
+polyppvt_b1_config = {
+    "patch_size" : 4, 
+    "embed_dims" : [64, 128, 320, 512], 
+    "num_heads" : [1, 2, 5, 8], 
+    "mlp_ratios" : [8, 8, 4, 4],
+    "depths" : [2, 2, 2, 2], 
+    "sr_ratios" : [8, 4, 2, 1],
+    "num_classes":1
+}
+
+polyppvt_b2_config = {
+    "patch_size" : 4, 
+    "embed_dims": [64, 128, 320, 512], 
+    "num_heads" : [1, 2, 5, 8], 
+    "mlp_ratios" : [8, 8, 4, 4],
+    "depths" : [3, 4, 6, 3], 
+    "sr_ratios" : [8, 4, 2, 1],
+    "num_classes":1
+}
+        
+
+polyppvt_b3_config = {
+    "patch_size" : 4, 
+    "embed_dims": [64, 128, 320, 512], 
+    "num_heads" : [1, 2, 5, 8], 
+    "mlp_ratios" : [8, 8, 4, 4],
+    "depths" : [3, 4, 18, 3], 
+    "sr_ratios" : [8, 4, 2, 1],
+    "num_classes":1
+}
+
+polyppvt_b4_config = {
+    "patch_size" : 4, 
+    "embed_dims": [64, 128, 320, 512], 
+    "num_heads" : [1, 2, 5, 8], 
+    "mlp_ratios" : [8, 8, 4, 4],
+    "depths" : [3, 8, 27, 3], 
+    "sr_ratios" : [8, 4, 2, 1],
+    "num_classes":1
+}
+
+polyppvt_b5_config = {
+    "patch_size" : 4, 
+    "embed_dims": [64, 128, 320, 512], 
+    "num_heads" : [1, 2, 5, 8], 
+    "mlp_ratios" : [4, 4, 4, 4], 
+    "depths" : [3, 6, 40, 3], 
+    "sr_ratios" : [8, 4, 2, 1],
+    "num_classes":1
+}
+
 def select_model(model_name, model_config):
     """
-    Unet expects image and masks to be same. 
+    Unet & Polyp-PVT expects image and masks to be same. 
     Segformer expects mask_size=image_size/4. 
     Make sure to change this in parameters.
     """
@@ -94,6 +162,20 @@ def select_model(model_name, model_config):
             'unet',
             **config
             )
+    elif model_name == "polyp_pvt":
+        polyppvt_configs  = {
+            "b0": polyppvt_b0_config,
+            "b1": polyppvt_b1_config,
+            "b2": polyppvt_b2_config,
+            "b3": polyppvt_b3_config,
+            "b4": polyppvt_b4_config,
+            "b5": polyppvt_b5_config
+            } 
+        
+        config = polyppvt_configs[model_config]
+        encoder_backbone = PyramidVisionTransformerImpr(**config)
+        model = PolypPVT(encoder_backbone)
+
     else:
         segformer_configs = {
             "b0": segformer_b0_config,
@@ -112,14 +194,14 @@ def select_model(model_name, model_config):
     return model
 
     
-    
-if __name__=="__main__":
-    model = select_model(model_name="segformer", model_config="b3")
-    learnable_params = sum(p.numel() for p in model.parameters() if p.requires_grad) / 1e6
-    print(f"Number of learnable parameters: {learnable_params:.2f} million")
-    
-    ## SEGFORMER outputs unbounded raw logits, Unet outputs bounded probailties [0, 1]
+def count_params(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad) / 1e6
 
-    model = select_model(model_name="unet", model_config="114")
-    learnable_params = sum(p.numel() for p in model.parameters() if p.requires_grad) / 1e6
-    print(f"Number of learnable parameters: {learnable_params:.2f} million")
+if __name__=="__main__":
+    for i in range(6):
+        model = select_model(model_name="polyp_pvt", model_config=f"b{i}")
+        print(f"Number of learnable parameters: {count_params(model):.2f} million")
+        dummy_input = torch.randn(1, 3, 384, 384)
+        with torch.no_grad():
+            output1, output2 = model(dummy_input)
+            print(output1.shape, output2.shape)

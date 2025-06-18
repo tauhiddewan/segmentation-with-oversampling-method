03f9649ef452c6b95b09145ca7cf7180460f8c66
tauhiddewan <dewan.tauhid.rahman@gmail.com>
2025-06-17 20:38:37 -0400

    minor bug fixed

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,5 +1,4 @@
 import torch
-# from torchmetrics import Dice, JaccardIndex
 
 def calculate_dice_score(preds, targets, device, model_name):
     if model_name!="unet":
@@ -26,22 +25,3 @@ def calculate_iou_score(preds, targets, device, model_name):
     fn = torch.sum((1-preds) * targets)
     denominator = (tp + fp + fn)
     return tp / denominator if denominator != 0 else torch.tensor(0.0, device=device)
-
-
-# def calculate_dice_score_pt(preds, targets, device):
-#     preds = torch.sigmoid(preds)
-#     preds = (preds > 0.5).float().to(device)
-#     targets = (targets > 0.5).float().to(device)
-#     targets = targets.to(torch.int32)
-#     scorer = Dice(average='micro', threshold=0.5).to(device)
-#     dice_score = scorer(preds, targets)
-#     return dice_score
-
-# def calculate_iou_score_pt(preds, targets, device):
-#     preds = torch.sigmoid(preds)
-#     preds = (preds > 0.5).float().to(device)
-#     targets = (targets > 0.5).float().to(device)
-#     targets = targets.to(torch.int32)
-#     scorer = JaccardIndex(task='binary', threshold=0.5).to(device)
-#     iou_score = scorer(preds, targets)
-#     return iou_score

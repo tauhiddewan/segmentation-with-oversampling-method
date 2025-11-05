25eb3a63ade5d0d573c433db4fbb83d13ac5ef46
tauhiddewan <dewan.tauhid.rahman@gmail.com>
2025-11-04 19:03:07 -0500

    fixed logfile creation issue

--- utils/misc.py
+++ utils/misc.py
@@ -1,11 +1,11 @@
 import atexit
 import logging
 
-def create_logger(log_filename, env_vars):
+def create_logger(log_filename, file_dir):
     logger = logging.getLogger('my_logger')
     logger.setLevel(logging.INFO) 
     formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-    file_handler = logging.FileHandler(f'{env_vars["output_folder_path"]}/{log_filename}.log')
+    file_handler = logging.FileHandler(f'{file_dir}/{log_filename}.log')
     file_handler.setFormatter(formatter)
     file_handler.setLevel(logging.INFO)
     logger.addHandler(file_handler)
